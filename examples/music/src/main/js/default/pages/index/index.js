2f06d2cfc59fe14baf8953c23acbf21c05f47652
重发激活邮件 <315038114@qq.com>
2020-09-11 17:02:44 +0800

    整理样式

--- examples/music/src/main/js/default/pages/index/index.js
+++ examples/music/src/main/js/default/pages/index/index.js
@@ -29,48 +29,48 @@ export default {
     curMusicIndex: 0,  //music index
     playStatus: 2,  //0:play,1:pause,2:stop
     curMusic: {  //current music info
-      curMusicName: "",
-      songer: "",
-      playTime: "00:00",
-      allTime: "00:00"
+      curMusicName: '',
+      songer: '',
+      playTime: '00:00',
+      allTime: '00:00'
     },
     musicList: []
   },
   onInit() {
     //get music duration
     audio.onplay = () => {
-      console.log("onplay start");
+      console.log('onplay start');
       this.curMusic.allTime = this.timeChange(audio.duration);
       this.playStatus = 0;
     };
     //update play progress
     audio.ontimeupdate = () => {
-      console.log("ontime update:");
+      console.log('ontime update:');
       this.curMusic.playTime = this.timeChange(audio.currentTime);
-      console.log("ontime update:" + this.curMusic.playTime);
+      console.log('ontime update:' + this.curMusic.playTime);
       this.playProgress = Math.floor(audio.currentTime * 100 / audio.duration);
-      console.log("ontime update progress:" + this.playProgress + ",duration:" + audio.duration);
+      console.log('ontime update progress:' + this.playProgress + ',duration:' + audio.duration);
     };
     //play next song when meet an error
     audio.onerror = () => {
-      console.log("onerror");
+      console.log('onerror');
       this.changeSong();
     };
     //perform operations based on the playback mode.
     audio.onended = () => {
-      console.log("onend");
+      console.log('onend');
       this.playStatus = 2;
       //after played the last song in sequential mode ,stop playback
       if (this.playMode == 0 && this.curMusicIndex == (this.musicList.length - 1)) {
         this.playStatus = 2;
-        this.playImage = "common/ic_music_play.png";
-        console.log("played the last song in sequential mode");
+        this.playImage = 'common/ic_music_play.png';
+        console.log('played the last song in sequential mode');
         return;
       }
-      let mode = "";
+      let mode = '';
       if (this.playMode == 2) {
-        console.log("random");
-        mode = "random";
+        console.log('random');
+        mode = 'random';
       }
       this.changeSong(mode);
     };
@@ -80,7 +80,7 @@ export default {
     this.stopPlay();
   },
   clickListItem(index) {
-    console.log("listItemClick:start:" + index);
+    console.log('listItemClick:start:' + index);
     if (index == undefined || index == null) {
       index = 0;
     }
@@ -88,10 +88,10 @@ export default {
     this.playStatus = 2;
     this.playMusic();
     this.showMusicList = false;
-    console.log("listItemClick:end");
+    console.log('listItemClick:end');
   },
   changeProgress(value) {
-    console.log("changeProgress start:" + value.progress);
+    console.log('changeProgress start:' + value.progress);
     if (value == null || value.progress == null) {
       return;
     }
@@ -99,24 +99,24 @@ export default {
       return;
     }
     let val = Math.floor((value.progress / 100) * audio.duration);
-    console.log("changeProgress change:" + val);
+    console.log('changeProgress change:' + val);
     audio.currentTime = val;
-    console.log("changeProgress end:");
+    console.log('changeProgress end:');
   },
   playMusic() {
-    console.log("playMusic:start");
-    //play music when is paused
-    this.playImage = "common/ic_music_pause.png";
+    console.log('playMusic:start');
+    //play music when paused
+    this.playImage = 'common/ic_music_pause.png';
     if (this.playStatus == 1) {
       audio.play();
       this.playStatus = 0;
-      console.log("playMusic:play");
+      console.log('playMusic:play');
       return;
     } else if (this.playStatus == 0) {
-      this.playImage = "common/ic_music_play.png";
+      this.playImage = 'common/ic_music_play.png';
       audio.pause();
       this.playStatus = 1;
-      console.log("playMusic:pause");
+      console.log('playMusic:pause');
       return;
     }
     if (this.curMusicIndex == null) {
@@ -128,18 +128,18 @@ export default {
       this.curMusic.curMusicName = music.name;
       this.curMusic.songer = music.songer;
       audio.src = music.src;
-      console.log("playMusic:src:" + music.src);
+      console.log('playMusic:src:' + music.src);
       audio.play();
       if (this.playMode == 3) {
           audio.loop = true;
       } else {
           audio.loop = false;
       }
-      console.log("playMusic:end");
+      console.log('playMusic:end');
     }
   },
   stopPlay() {
-    console.log("stopPlay");
+    console.log('stopPlay');
     audio.src = null;
     audio.onstop = null;
     audio.onplay = null;
@@ -147,7 +147,7 @@ export default {
     audio.onerror = null;
   },
   changeMode() {
-    console.log("changeMode start");
+    console.log('changeMode start');
     if (this.playMode == 3) {
       this.playMode = 0;
     } else {
@@ -158,51 +158,51 @@ export default {
     } else {
       audio.loop = false;
     }
-    console.log("changeMode:" + this.playMode);
+    console.log('changeMode:' + this.playMode);
     this.modeImage = modeImages[this.playMode];
   },
   changeSong(mode) {
-    console.log("ChangeSong:start");
+    console.log('ChangeSong:start');
     if (this.playStatus == 0 || this.playStatus == 1) {
       this.playStatus = 2;
     }
     if (this.playMode == 2) {
-      mode = "random";
+      mode = 'random';
     }
     //previous
-    if (mode == "previous") {
+    if (mode == 'previous') {
       this.curMusicIndex == 0 ? (this.curMusicIndex = (this.musicList.length - 1)) : this.curMusicIndex--;
-      console.log("ChangeSong:previous:" + this.curMusicIndex);
-    } else if (mode == "random") {  //random
+      console.log('ChangeSong:previous:' + this.curMusicIndex);
+    } else if (mode == 'random') {  //random
       this.curMusicIndex = parseInt(Math.random() * (this.musicList.length), 10);
-      console.log("ChangeSong:random:" + this.curMusicIndex);
+      console.log('ChangeSong:random:' + this.curMusicIndex);
     } else {  //next
       (this.curMusicIndex == this.musicList.length - 1) ? this.curMusicIndex = 0 : this.curMusicIndex++;
-      console.log("ChangeSong:next" + this.curMusicIndex);
+      console.log('ChangeSong:next' + this.curMusicIndex);
     }
     this.curMusic.curMusicName = this.musicList[this.curMusicIndex].name;
     this.curMusic.songer = this.musicList[this.curMusicIndex].songer;
     this.playMusic(this.curMusicIndex);
     if(this.showMusicList){
-      this.$refs.musicList.scrollTo({ index: (this.curMusicIndex == 0?0:this.curMusicIndex-1) });
+      this.$refs.musicList.scrollTo({ index: (this.curMusicIndex == 0 ? 0:this.curMusicIndex-1) });
     }
-    console.log("ChangeSong:end");
+    console.log('ChangeSong:end');
   },
   timeChange(seconds) {
-    console.log("timeChange:start:" + seconds);
+    console.log('timeChange:start:' + seconds);
     if (seconds == null || seconds < 0) {
-      return "00:00";
+      return '00:00';
     }
     let minute, second = 0;
     minute = Math.floor(seconds / 60);
     second = Math.floor(seconds % 60);
-    let result = (minute > 9 ? minute : "0" + minute) + ":" + (second > 9 ? second : "0" + second);
-    console.log("timeChange:end:" + result);
+    let result = (minute > 9 ? minute : '0' + minute) + ':' + (second > 9 ? second : '0' + second);
+    console.log('timeChange:end:' + result);
     return result;
   },
   changePage(operation) {
-    console.log("changePage:start");
-    if (operation != null && operation == "close") {
+    console.log('changePage:start');
+    if (operation != null && operation == 'close') {
       this.showMusicList = false;
       return;
     }
@@ -212,13 +212,13 @@ export default {
     }
   },
   exitApp(){
-    console.log("start exit");
+    console.log('start exit');
     app.terminate();
-    console.log("end exit");
+    console.log('end exit');
   },
   swipeEvent(e){
-    if(e.direction == "right"){
-      console.log("start exit");
+    if(e.direction == 'right'){
+      console.log('start exit');
       this.exitApp();
     }
   },
@@ -226,7 +226,7 @@ export default {
       let that = this;
         //get all music file
         file.readText({
-            uri:"internal://app/musicList.txt",
+            uri:'internal://app/musicList.txt',
             success(data){
                 console.log(data.text);
                 let music = JSON.parse(data.text);
@@ -235,7 +235,7 @@ export default {
                 }
             },
             fail(data,code){
-                console.log("get music list failed :"+code);
+                console.log('get music list failed :'+code);
             }
         });
   }
