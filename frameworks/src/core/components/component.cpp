3826a1f15fc4c06700a85414de825a48eecc37fd
dml-hw <315038114@qq.com>
2021-07-29 09:21:09 +0000

    test

--- frameworks/src/core/components/component.cpp
+++ frameworks/src/core/components/component.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 #include "component.h"
 #include "ace_log.h"
 #include "ace_mem_base.h"
