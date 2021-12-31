6588a99ba907f1a88d68310c04197618f4cca2d4
xvcheng1979101 <xvcheng1979@qq.com>
2021-12-31 05:05:41 +0000

    !13 更新图形系统中UICanva画布API功能3.0分支代码到3.0分支 更新图形系统中UICanva画布API功能3.0分支代码到3.0分支 RM008 对标H5，支持实现如下属性及接口 RM009 线条样式属性及矩形方法 RM011 在画布上绘制文本（无填充）以及向画布上绘制图像 RM012 支持alpha和合成模式属性 RM013 渐变填充方法的实现
    描述（做了什么，变更了什么）
    更新图形系统中UICanva画布API功能3.0分支代码到3.0分支
    RM008 对标H5，支持实现如下属性及接口
    RM009 线条样式属性及矩形方法
    RM011 在画布上绘制文本（无填充）以及向画布上绘制图像
    RM012 支持alpha和合成模式属性
    RM013 渐变填充方法的实现
    
    Signed-off-by: xvcheng1979101 <xvcheng1979@qq.com>

--- frameworks/src/core/base/js_fwk_common.cpp
+++ frameworks/src/core/base/js_fwk_common.cpp
@@ -1313,5 +1313,53 @@ uint16_t ParseKeyIdFromJSString(const jerry_value_t str)
     }
     return keyId;
 }
+
+int8_t ParseLineCap(const char *lineCap)
+{
+    if(lineCap == nullptr){
+        return -1;
+    }
+    if(strcasecmp(lineCap, LINECAP_BUTT) == 0){
+        return BUTT_VALUE;
+    }
+    if(strcasecmp(lineCap, LINECAP_SQUARE) == 0){
+        return SQUARE_VALUE;
+    }
+    if(strcasecmp(lineCap, LINECAP_ROUND) == 0){
+        return ROUND_VALUE;
+    }
+    return -1;
+}
+
+int8_t ParseLineJoin(const char *lineJoin)
+{
+    if(lineJoin == nullptr){
+        return -1;
+    }
+    if(strcasecmp(lineJoin, LINEJOIN_MITER) == 0){
+        return LINEJOIN_MITER_VALUE;
+    }
+    if(strcasecmp(lineJoin, LINEJOIN_ROUND) == 0){
+        return LINEJOIN_ROUND_VALUE;
+    }
+    if(strcasecmp(lineJoin, LINEJOIN_BEVEL) == 0){
+        return LINEJOIN_BEVEL_VALUE;
+    }
+    return -1;
+}
+
+int16_t ParseImageSize(jerry_value_t value)
+{
+    int16_t val = 0;
+    if(jerry_value_is_number(value)){
+        val = jerry_get_number_value(value);
+    }else if(jerry_value_is_string(value)){
+        char* str = MallocStringOf(value);
+        val = atoi(str);
+        ACE_FREE(str);
+    }
+    return val;
+}
+
 } // namespace ACELite
 } // namespace OHOS
